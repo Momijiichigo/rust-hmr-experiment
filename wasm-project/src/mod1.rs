9bb777620b7f0a116779b773c21b0ed1d020cd29
Momijiichigo <ichigomomiji436@gmail.com>
2024-01-17 18:00:03 -0600

    progress on error investigation

--- wasm-project/src/mod1.rs
+++ wasm-project/src/mod1.rs
@@ -1,7 +1,7 @@
 use js_sys::{Function, Object, Reflect, WebAssembly};
+use leptos::*;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::{spawn_local, JsFuture};
-use leptos::*;
 
 #[wasm_bindgen]
 extern "C" {
@@ -38,18 +38,33 @@ pub fn __Comp1() -> impl IntoView {
         }
     }
 }
+pub fn test_comp() -> impl IntoView {
+    {
+        {
+            let _ = ::leptos::leptos_dom::html::div;
+            ::leptos::leptos_dom::html::div()
+        }
+    }
+}
 #[wasm_bindgen]
 pub fn ComponentA_into_view() -> _View {
     log!("Before exec");
-    let _ = leptos_reactive::untrack_with_diagnostics(|| {
-        ComponentA()
-    });
+    let val = leptos_dom::Component::new("test", test_comp).into_view();
+    log!("{:?}", val);
+    // let _ = leptos_reactive::untrack_with_diagnostics(|| {
+    //     ComponentA()
+    // });
     log!("After exec");
     _View {
         view: ComponentA().into_view(),
     }
 }
 
+pub fn test1<T>(f: impl FnOnce() -> T) -> T {
+    let result = f();
+
+    result
+}
 #[wasm_bindgen]
 pub struct _View {
     view: View,
--- wasm-project/web-public/index.js
+++ wasm-project/web-public/index.js
@@ -14,36 +14,59 @@ async function run() {
     __wbindgen_externref_table_grow: (delta) => wasmTable.grow(delta),
     __wbindgen_externref_table_set_null: (idx) => wasmTable.set(idx),
   }
+  // ----------- debugging purpose --------------
+  hmr_import_obj.env = new Proxy(
+    {}, {
+      get(_target, prop, _receiver) {
+        if (prop.indexOf("leptos_dom::components::ComponentRepr::new_with_id_concrete") === 0) {
+          return (...args) => {
+            const result = project[prop](...args);
+            console.log("new_with_id_concrete called", args);
+            return result;
+          }
+        }
+        if (prop.indexOf("<leptos_dom::components::ComponentRepr as leptos_dom::IntoView>::into_view") === 0) {
+          return (...args) => {
+            console.log("ComponentRepr::into_view called", args);
+            return project[prop](...args);
+          }
+        }
 
+        if (prop in project) {
+          return project[prop];
+        }
+        if (prop === "__linear_memory") {
+          return project.memory;
+        }
+	if (prop === "__stack_pointer") {
+	  // TODO: using wrong value, need fix
+	  return new WebAssembly.Global({value: "i32", mutable: true}, window.heap_next);
+	}
+        if (prop === "__indirect_function_table") {
+          return wasmTable;
+        }
+
+        if (prop.includes("::describe::")) {
+          return () => {};
+        }
+        if (prop.indexOf("wasm_bindgen::__rt::link_mem_intrinsics") === 0) {
+          return () => {};
+        }
+    
+      }
+    }
+  );
+  // --------------------------------------------
+  /*
+  // ----------- production purpose --------------
   hmr_import_obj.env = new Proxy(
     project, {
       get(target, prop, _receiver) {
-	// --- debugging purpose ---
-	
-	if (prop.indexOf("leptos_dom::components::ComponentRepr::new_with_id_concrete") === 0) {
-	  console.log("new_with_id called");
-	  return (...args) => {
-	    console.log("new_with_id_concrete", args);
-	    return Reflect.get(...arguments)(...args);
-	  }
-	}
-	if (prop.indexOf("<leptos_dom::components::ComponentRepr as leptos_dom::IntoView>::into_view") === 0) {
-	  console.log("into_view called");
-	  return (...args) => {
-	    console.log("ComponentRepr::into_view", args);
-	    return Reflect.get(...arguments)(...args);
-	  }
-	}
-	// -------------------------
 	if (prop in target) {
       	  return Reflect.get(...arguments);
       	}
 	if (prop === "__linear_memory") {
 	  return project.memory;
-	}
-	if (prop === "__stack_pointer") {
-	  // TODO: using wrong value, need fix
-	  return new WebAssembly.Global({value: "i32", mutable: true}, window.heap_next);
 	}
 	if (prop === "__indirect_function_table") {
 	  return wasmTable;
@@ -58,6 +81,7 @@ async function run() {
       }
     }
   )
+  */
 
   hmr_import_obj.__wbindgen_placeholder__ = new Proxy(imports.wbg, {
     get(target, prop, _receiver) {
